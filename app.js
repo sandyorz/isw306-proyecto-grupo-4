40feb08cb424653552f98dce47c761e6042e7a55
Alex Santana <100074369@p.uapa.edu.do>
2026-06-06 23:56:47 -0400

    Detener el submit por defecto (Aporte Alex Santana)

--- app.js
+++ app.js
@@ -14,5 +14,45 @@ document.addEventListener('DOMContentLoaded', () => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
+     // --- Parte 4: Participación de Alex Santana (100074369) ---
+    // Resumen: Me encargué de detener el submit por defecto, guardar los datos en 
+    // LocalStorage con arreglos y mostrar la alerta verde si todo sale bien.
     
+    form.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+
+        const isEmailValid = validateEmail();
+        const isSeverityValid = validateSeverity();
+        const isModuleValid = validateModule();
+        const isDescValid = validateDescription();
+
+        if (isEmailValid && isSeverityValid && isModuleValid && isDescValid) {
+            
+            const newBugReport = {
+                id: Date.now(),
+                email: emailInput.value.trim(),
+                severity: severityInput.value,
+                module: moduleInput.value.trim(),
+                description: descInput.value.trim(),
+                date: new Date().toLocaleDateString()
+            };
+
+            let ticketsArray = JSON.parse(localStorage.getItem('bugTickets')) || [];
+            ticketsArray.push(newBugReport);
+            localStorage.setItem('bugTickets', JSON.stringify(ticketsArray));
+            
+            formAlert.innerHTML = `<strong>¡Perfecto!</strong> El bug en ${newBugReport.module} fue registrado con éxito.`;
+            formAlert.className = 'status-alert success';
+            formAlert.classList.remove('hidden');
+            
+            form.reset(); 
+            
+            setTimeout(() => { formAlert.classList.add('hidden'); }, 4000);
+            
+        } else {
+            formAlert.innerHTML = `<strong>Atención:</strong> Tienes errores en el formulario, revisa los campos en rojo.`;
+            formAlert.className = 'status-alert';
+            formAlert.classList.remove('hidden');
+        }
+    });
 });
