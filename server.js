3acff28484aecd74e6311bc88879209976087c22
Alexander Tejeda <100074246@p.uapa.edu.do>
2026-06-24 22:49:17 -0400

    crear endpoints api get y post (Aporte Alexander)

--- server.js
+++ server.js
@@ -27,3 +27,28 @@ const db = new sqlite3.Database('./devops_dashboard.db', (err) => {
         )`);
     }
 });
+
+// --- Participación de Alexander Tejeda (100074246) ---
+// Resumen: Implementación de los endpoints de la API. GET para listar incidencias 
+// y POST para registrar nuevas desde el formulario del frontend.
+
+app.get('/api/bugs', (req, res) => {
+    const sql = `SELECT * FROM bugs ORDER BY id DESC LIMIT 5`; 
+    db.all(sql, [], (err, rows) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(rows);
+    });
+});
+
+app.post('/api/bugs', (req, res) => {
+    const { email, severity, module, description } = req.body;
+    const sql = `INSERT INTO bugs (email, severity, module, description) VALUES (?, ?, ?, ?)`;
+    db.run(sql, [email, severity, module, description], function(err) {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json({ message: "Guardado exitoso", bugId: this.lastID });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor backend corriendo en http://localhost:${PORT}`);
+});
